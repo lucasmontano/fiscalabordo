b0b65c8dc7bb71a0bcfcf2472a64edbda40d41f8
Lucas Montano <lucasmontano@gmail.com>
2015-01-16 00:14:57 -0200

    ButterKnife, the magic

--- app/src/main/java/br/com/moolab/fiscalabordo/fragments/ConfirmDialogFragment.java
+++ app/src/main/java/br/com/moolab/fiscalabordo/fragments/ConfirmDialogFragment.java
@@ -1,6 +1,7 @@
 package br.com.moolab.fiscalabordo.fragments;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
 import android.app.Dialog;
 import android.graphics.Typeface;
@@ -18,6 +19,9 @@ import android.widget.TextView;
 
 import br.com.moolab.fiscalabordo.utils.FontsUtils;
 import br.com.moolab.fiscalabordo.R;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 
 public class ConfirmDialogFragment extends DialogFragment {
 
@@ -27,12 +31,20 @@ public class ConfirmDialogFragment extends DialogFragment {
     public static final String ARG_STAND = "ARG_STAND";
     public static final String ARG_BROKE = "ARG_BROKE";
 
-    private Builder mBuilder;
     private ConfirmCallback mCallback;
-    private EditText company;
-    private TextView velocity;
-    private Switch facebookLogin;
-    private TextView publicData;
+
+    @InjectView(R.id.company) EditText company;
+    @InjectView(R.id.velocity) TextView velocity;
+    @InjectView(R.id.public_data) TextView publicData;
+    @InjectView(R.id.facebook) Switch facebookLogin;
+
+    @InjectView(R.id.is_belt) ImageView isBelt;
+    @InjectView(R.id.is_stand) ImageView isStand;
+    @InjectView(R.id.is_bug) ImageView isBug;
+    @InjectView(R.id.is_broke) ImageView isBroke;
+
+    private Typeface robotoRegular;
+    private Typeface robotoBoldCondensed;
 
     public interface ConfirmCallback {
         public void onConfirm(String company, String velocity, Boolean belt, Boolean broke, Boolean bug, Boolean stand, boolean user);
@@ -42,6 +54,32 @@ public class ConfirmDialogFragment extends DialogFragment {
     public void onAttach(Activity activity) {
         super.onAttach(activity);
         mCallback = (ConfirmCallback) activity;
+        robotoRegular = FontsUtils.getInstance().getRobotoRegular(activity.getAssets());
+        robotoBoldCondensed = FontsUtils.getInstance().getRobotoBoldCondensed(activity.getAssets());
+    }
+
+    @OnClick(R.id.action_ok)
+    public void ok(View view) {
+        mCallback.onConfirm(
+                company.getText().toString(),
+                velocity.getText().toString(),
+                getArguments().getBoolean(ARG_BELT),
+                getArguments().getBoolean(ARG_BROKE),
+                getArguments().getBoolean(ARG_BUG),
+                getArguments().getBoolean(ARG_STAND),
+                facebookLogin.isChecked()
+        );
+        ConfirmDialogFragment.this.dismiss();
+    }
+
+    @OnClick(R.id.action_cancel)
+    public void cancel(View view) {
+        ConfirmDialogFragment.this.dismiss();
+    }
+
+    @OnClick(R.id.facebook_row)
+    public void checkLogin(View view) {
+        facebookLogin.setChecked(!facebookLogin.isChecked());
     }
 
     @Override
@@ -49,58 +87,26 @@ public class ConfirmDialogFragment extends DialogFragment {
 
         LinearLayout viewInflated = (LinearLayout) getActivity().getLayoutInflater().inflate(R.layout.dialog_confirm, null);
 
+        Bundle arguments = getArguments();
 
-        velocity = ((TextView) viewInflated.findViewById(R.id.velocity));
-        velocity.setTypeface(FontsUtils.getInstance().getRobotoBoldCondensed(getActivity().getAssets()));
-        velocity.setText(getArguments().getString(ARG_VELOCITY));
+        // The Magic
+        ButterKnife.inject(this, viewInflated);
 
-        ((ImageView) viewInflated.findViewById(R.id.is_belt)).setVisibility(getArguments().getBoolean(ARG_BELT) ? View.VISIBLE : View.GONE);
-        ((ImageView) viewInflated.findViewById(R.id.is_stand)).setVisibility(getArguments().getBoolean(ARG_STAND) ? View.VISIBLE : View.GONE);
-        ((ImageView) viewInflated.findViewById(R.id.is_bug)).setVisibility(getArguments().getBoolean(ARG_BUG) ? View.VISIBLE : View.GONE);
-        ((ImageView) viewInflated.findViewById(R.id.is_broke)).setVisibility(getArguments().getBoolean(ARG_BROKE) ? View.VISIBLE : View.GONE);
+        // Bus Velocity
+        velocity.setText(arguments.getString(ARG_VELOCITY));
 
-        Typeface robotoRegular = FontsUtils.getInstance().getRobotoRegular(getActivity().getAssets());
+        // Options Selected
+        isBelt.setVisibility(arguments.getBoolean(ARG_BELT) ? View.VISIBLE : View.GONE);
+        isStand.setVisibility(arguments.getBoolean(ARG_STAND) ? View.VISIBLE : View.GONE);
+        isBug.setVisibility(arguments.getBoolean(ARG_BUG) ? View.VISIBLE : View.GONE);
+        isBroke.setVisibility(arguments.getBoolean(ARG_BROKE) ? View.VISIBLE : View.GONE);
 
-        publicData = (TextView) viewInflated.findViewById(R.id.public_data);
+        // Fonts
+        velocity.setTypeface(robotoBoldCondensed);
         publicData.setTypeface(robotoRegular);
-
-        company = ((EditText) viewInflated.findViewById(R.id.company));
         company.setTypeface(robotoRegular);
 
-
-        facebookLogin = ((Switch) viewInflated.findViewById(R.id.facebook));
-        ((View) facebookLogin.getParent()).setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                facebookLogin.setChecked( ! facebookLogin.isChecked());
-            }
-        });
-
-        ((Button) viewInflated.findViewById(R.id.action_ok)).setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mCallback.onConfirm(
-                        company.getText().toString(),
-                        velocity.getText().toString(),
-                        getArguments().getBoolean(ARG_BELT),
-                        getArguments().getBoolean(ARG_BROKE),
-                        getArguments().getBoolean(ARG_BUG),
-                        getArguments().getBoolean(ARG_STAND),
-                        facebookLogin.isChecked()
-                );
-                ConfirmDialogFragment.this.dismiss();
-            }
-        });
-
-
-        ((Button) viewInflated.findViewById(R.id.action_cancel)).setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ConfirmDialogFragment.this.dismiss();
-            }
-        });
-
-        mBuilder = new Builder(new ContextThemeWrapper(getActivity(), R.style.Theme_AppCompat_Light));
+        Builder mBuilder = new Builder(new ContextThemeWrapper(getActivity(), R.style.Theme_AppCompat_Light));
         mBuilder.setView(viewInflated);
 
         return mBuilder.create();
